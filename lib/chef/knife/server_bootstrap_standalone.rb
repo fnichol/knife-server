14f6239e1f1e4bb008afacd7120161fb3d7350d1
Erik Hollensbe <erik+github@hollensbe.org>
2013-03-25 08:38:00 -0700

    yay rspec

--- lib/chef/knife/server_bootstrap_standalone.rb
+++ lib/chef/knife/server_bootstrap_standalone.rb
@@ -55,7 +55,7 @@ class Chef
         ENV['WEBUI_PASSWORD'] = config[:webui_password] || options[:webui_password][:default]
         ENV['AMQP_PASSWORD'] = config[:amqp_password] || options[:amqp_password][:default]
 
-        ENV['NO_TEST'] = 1 if config[:no_test]
+        ENV['NO_TEST'] = "1" if config[:no_test]
 
         bootstrap = Chef::Knife::Bootstrap.new
         bootstrap.name_args = [ config[:host] ]
